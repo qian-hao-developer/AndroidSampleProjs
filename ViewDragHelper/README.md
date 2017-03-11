9ad94c64e00b86e055da8473e4b09d49b7c4c402
qian.hao <qian.hao.developer@gmail.com>
2017-03-11 19:45:23 +0900

    change name of ViewDragHelperSampleProj

---
+++ ViewDragHelper/README.md
@@ -0,0 +1,8 @@
+drager
+
+-
+
+ViewDragHelper demo.
+
+
+![](https://github.com/gotokatsuya/drager/blob/master/doc/drager-sample3.gif)
---
+++ ViewDragHelper/app/src/main/java/com/goka/drager/MainActivity.java
@@ -0,0 +1,39 @@
+package com.goka.drager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * 3 sample main view
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.sample1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity1.class));
+            }
+        });
+
+        findViewById(R.id.sample2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+
+        findViewById(R.id.sample3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity3.class));
+            }
+        });
+    }
+}
